09b73efe0805f136763723ff9287ffff8eab35c0
iRebeca <birleanurebeca@yahoo.com>
2020-10-28 17:56:48 +0200

    adaugat meniu generic

---
+++ Assets/Type2Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Type2Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Type1-first");
+    }
+}
